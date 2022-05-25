6e3c99416c4741ca3248ccabd1c12c38484e8bb4
unknown <karenchis2000@hotmail.com>
2022-05-24 20:30:26 -0500

    Actividad

---
+++ JavaScript/script.js
@@ -0,0 +1,106 @@
+$('.contenido').mouseover(function(){
+    $('.contenido').css('background', '#ece5ce')
+})
+
+$('.contenido').mouseout(function(){
+    $('.contenido').css('background', ' #e5d9c6')
+})
+
+$('.pm').click(function(){
+    $('.pm').css('font-size', '20px')
+})
+
+$('.pm').dblclick(function(){
+    $('.pm').css('color', '#bf9780')
+})
+
+$('.peu').click(function(){
+    $('.peu').css('font-size', '20px')
+})
+
+$('.peu').dblclick(function(){
+    $('.peu').css('color', '#bf9780')
+})
+
+$('.pc').click(function(){
+    $('.pc').css('font-size', '20px')
+})
+
+$('.pc').dblclick(function(){
+    $('.pc').css('color', '#bf9780')
+})
+
+$('.pb').click(function(){
+    $('.pb').css('font-size', '20px')
+})
+
+$('.pb').dblclick(function(){
+    $('.pb').css('color', '#bf9780')
+})
+
+$('.pj').click(function(){
+    $('.pj').css('font-size', '20px')
+})
+
+$('.pj').dblclick(function(){
+    $('.pj').css('color', '#bf9780')
+})
+
+$('.pi').click(function(){
+    $('.pi').css('font-size', '20px')
+})
+
+$('.pi').dblclick(function(){
+    $('.pi').css('color', '#bf9780')
+})
+
+$('.pe').click(function(){
+    $('.pe').css('font-size', '20px')
+})
+
+$('.pe').dblclick(function(){
+    $('.pe').css('color', '#bf9780')
+})
+
+$('.pa').click(function(){
+    $('.pa').css('font-size', '20px')
+})
+
+$('.pa').dblclick(function(){
+    $('.pa').css('color', '#bf9780')
+})
+
+$('.pru').click(function(){
+    $('.pru').css('font-size', '20px')
+})
+
+$('.pru').dblclick(function(){
+    $('.pru').css('color', '#bf9780')
+})
+
+$('.pcol').click(function(){
+    $('.pcol').css('font-size', '20px')
+})
+
+$('.pcol').dblclick(function(){
+    $('.pcol').css('color', '#bf9780')
+})
+
+$('.lista').mouseover(function(){
+    $('.lista').css('background', '#ece5ce')
+})
+
+$('.lista').mouseout(function(){
+    $('.lista').css('background', '#e4d5bb')
+})
+
+$('.final').mouseover(function(){
+    $('.final').css('background', '#ece5ce')
+})
+
+$('.final').mouseout(function(){
+    $('.final').css('background', ' #e4d5bb')
+})
+
+alert('“La gente tiende a ser más inteligente emocionalmente a medida que crecen y maduran”.');
+prompt('Deje su correo para contactarlo');
